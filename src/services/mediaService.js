bc05abca5b0a26ccde341d68d5323f8db933b736
ahmed dousa <ahmed.dousa@mazruiinternational.ae>
2021-12-14 01:11:39 +0400

    fix baseurl and environment variables check

--- src/services/mediaService.js
+++ src/services/mediaService.js
@@ -1,7 +1,12 @@
 const getHeaderAuthorization = () => {
-  const username = process.env.BASIC_AUTH_USER;
-  const password = process.env.BASIC_AUTH_PASS;
+  const username = process.env.REACT_APP_BASIC_AUTH_USER;
+  const password = process.env.REACT_APP_BASIC_AUTH_PASS;
   return `Basic ${window.btoa(`${username}:${password}`)}`;
+};
+
+const getAbsoluteUrl = (url) => {
+  const baseURL = process.env.BASE_URL || "http://localhost:3000/";
+  return `${baseURL}${url}`;
 };
 
 const getMedia = (query, page = 1, pageSize = 15) => {
@@ -12,10 +17,12 @@ const getMedia = (query, page = 1, pageSize = 15) => {
       Authorization: getHeaderAuthorization(),
     },
   };
-  return fetch(
-    `http://localhost:3002/media?search=${query}&page=${page}&pageSize=${pageSize}`,
-    requestOptions
-  );
+  const params = { page, pageSize };
+  if (query) {
+    params.search = query;
+  }
+  const queryString = new URLSearchParams(params).toString();
+  return fetch(getAbsoluteUrl(`media?${queryString}`), requestOptions);
 };
 
 export const mediaService = {
